3a48c9a9a4a2dcc419f719df732d865e0a2ceff4
Chris Reid <chris@raygun6.com>
2024-11-27 17:08:28 -0800

    print article titles and descriptions

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 import requests
 
-# https://newsapi.org
-
 api_key = os.getenv("API_KEY")
 url = "https://newsapi.org/v2/top-headlines" \
     "?country=us&category=business&" \
     "apiKey=" + api_key
 
+# Make request
 r = requests.get(url)
-content = r.text
-print(content)
+
+# Get dictionary with data
+content = r.json()
+
+# Access article titles and description 
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
